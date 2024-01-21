d73dd0c4105aebd4d66301942a209e3d8dcf994d
Cristian Paolini <cristianpaolini3@gmail.com>
2024-01-21 13:20:48 -0300

    feat: implement useChatSocket hook for real-time updates
    This commit adds the useChatSocket hook to handle real-time updates for chat messages. It utilizes the @tanstack/react-query and socket.io to synchronize data between the server and the client.
    
    - Implement useChatSocket hook for real-time updates
    - Listen for socket events (addKey, updateKey) and update React Query data accordingly
    - Ensure proper cleanup of socket event listeners on component unmount

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -9,6 +9,7 @@ import { Member, MemberRole, Profile } from "@prisma/client";
 import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useRouter, useParams } from "next/navigation";
 
 import { UserAvatar } from "@/components/user-avatar";
 import { ActionTooltip } from "@/components/action-tooltip";
@@ -64,6 +65,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false);
     const { onOpen } = useModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const onMemberClick = () => {
+        if (member.id === currentMember.id) {
+            return;
+        }
+
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+    }
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -121,7 +132,7 @@ export const ChatItem = ({
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-                <div className="cursor-pointer hover:drop-shadow-md transition">
+                <div onClick={onMemberClick} className="cursor-pointer hover:drop-shadow-md transition">
                     <UserAvatar src={member.profile.imageUrl} />
                 </div>
                 <div className="flex flex-col w-full">

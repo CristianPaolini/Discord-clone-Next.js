4556efbf8f0ece3ffa8cbbe4aed257eff1cedeaf
Cristian Paolini <cristianpaolini3@gmail.com>
2024-01-21 00:07:24 -0300

    Update [messageId].ts

--- pages/api/socket/messages/[messageId].ts
+++ pages/api/socket/messages/[messageId].ts
@@ -141,6 +141,6 @@ export default async function handler(
         return res.status(200).json(message);
     } catch (error) {
         console.log("[MESSAGE_ID]", error);
-        return
+        return res.status(500).json({ error: "Internal Error" });
     }
 }
